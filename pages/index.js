f353b53eed6b8cb2b2ee27832bbaa905caf2819d
Alfred Lua <alfredlua@gmail.com>
2022-10-27 17:06:30 +0800

    Fix app

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,22 @@
-import PluginClient from '@dashibase/plugin-client'
+import { useEffect } from 'react'
 
 export default function Home() {
-  const client = new PluginClient()
+  async function importClient() {
+    const PluginClient = (await import('@dashibase/plugin-client')).default
+    const client = new PluginClient()
+
+    client.onSetup((data) => {
+      // Prints a log when a SETUP message is received  
+      console.log(`Received SETUP message ${JSON.stringify(data)}`)
+    })
+    
+    // Inform Dashibase that plugin is ready to be setup
+    client.init()
+  }
   
-  client.onSetup((data) => {
-    // Prints a log when a SETUP message is received  
-    console.log(`Received SETUP message ${JSON.stringify(data)}`)
-  })
-  
-  // Inform Dashibase that plugin is ready to be setup
-  client.init()
+  useEffect(() => {
+    importClient()
+  }, [])
 
   return (
     <div>Hello World!</div>
